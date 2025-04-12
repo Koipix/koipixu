e417112e68ea058893928d62d5cf792b87e5d763
Koipix <koi.devt@gmail.com>
2025-04-13 02:07:57 +0800

    home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './component/home.tsx'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
                     <a href="https://github.com/koipix" className="inline-flex items-center">
                         <img src="https://avatars.githubusercontent.com/u/92094832?v=4" 
                              alt="miii"
-                             className="h-28 rounded-xl mr-8"/>
+                             className="h-24 rounded-xl mr-8"/>
                         <span className="text-gray-200 text-3xl">
                             Koi :3
                         </span>
@@ -24,26 +25,7 @@ function App() {
                     </nav>
                   </div>
                 </header>
-                <main>
-                    <section>
-                        <h1 className="text-white text-3xl font-semibold">
-                            Hi, I'm
-                            <div className="mt-3 text-pink-300">
-                                <span className="text-base text-gray-400">(ja) </span>
-                                コイ
-                                <br/>
-                                <span className="text-base text-gray-400">(ch) </span>
-                                鲤
-                                <br/>
-                                <span className="text-base text-gray-400">(en) </span>
-                                Koi
-                            </div>
-                        </h1>
-                        <p className="text-2xl text-gray-200 mt-10 font-medium">
-                            An aspiring student currently working on small projects specially game-dev and also anime art!
-                        </p>
-                    </section>
-                </main>
+                <Home/>
             </div>
         </div>
     </>
---
+++ src/component/home.tsx
@@ -0,0 +1,31 @@
+function Home() {
+    return (
+        <section>
+            <h1 className="text-white text-3xl font-semibold">
+                Hi, I'm
+                <div className="mt-3 text-pink-300">
+                    <span className="text-base text-gray-400">(ja) </span>
+                    コイ
+                    <br/>
+                    <span className="text-base text-gray-400">(ch) </span>
+                    鲤
+                    <br/>
+                    <span className="text-base text-gray-400">(en) </span>
+                    Koi
+                </div>
+            </h1>
+            <p className="text-2xl text-gray-200 mt-10 font-medium">
+                An aspiring student currently working on small projects specially game-dev and also anime art!
+            </p>
+            <div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Home
