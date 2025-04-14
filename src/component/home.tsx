317340e924455f0847250365200c88e3c687a476
Koipix <koi.devt@gmail.com>
2025-04-14 22:24:04 +0800

    Project section

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -3,10 +3,11 @@ import { faGithubAlt, faXTwitter, faItchIo } from '@fortawesome/free-brands-svg-
 
 function Home() {
     return (
+      <>
         <section>
             <h1 className="text-white text-3xl font-semibold">
                 Hi, I'm
-                <div className="mt-3 text-pink-300">
+                <div className="mt-3 text-pink-200">
                     <span className="text-base text-gray-400">(ja) </span>
                     コイ
                     <br/>
@@ -43,6 +44,36 @@ function Home() {
                 </ul>
             </div>
         </section>
+        <section className="mt-15">
+          <h1 className="text-3xl mb-5"> Projects </h1>
+          <dl className="grid grid-cols-2 gap-4">
+            <div className="rounded-lg bg-mantle p-4 text-accent shadow-md transition-shadow">
+              <dt>
+                <span>
+                  <a className="text-2xl text-pink-300">
+                    Inevatable Fate
+                  </a>
+                </span>
+              </dt>
+              <dd className="mt-3 text-gray-200 text-lg">
+                A game project we made as a team in submission for Brackeys Jam 2025.1.1
+              </dd>
+            </div>
+            <div className="rounded-lg bg-mantle p-4 text-accent shadow-md transition-shadow">
+              <dt>
+                <span>
+                  <a className="text-2xl text-pink-300">
+                    Arona Discord Bot
+                  </a>
+                </span>
+              </dt>
+              <dd className="mt-3 text-gray-200 text-lg">
+                Arona is a flexible discord bot with fun features like minigames and integrated language model
+              </dd>
+            </div> 
+          </dl>
+        </section>
+      </>
     )
 }
 
