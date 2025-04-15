dc02af292860ae225ebe602bbc758553f8c8163e
Koipix <koi.devt@gmail.com>
2025-04-16 05:26:21 +0800

    blog

--- src/component/about.tsx
+++ src/component/about.tsx
@@ -1,6 +1,22 @@
 function About() {
   return (
-    <h1>Hello It's Me!</h1>
+    <>
+      <p className="text-gray-400 text-lg mb-5">April 16, 2025 &lt;3</p>
+      <img className="rounded-xl" src="/imgs/kana-bg.png" alt="konata"/>
+      <h1 className="text-4xl font-bold mb-15 mt-5 text-center">よろしくね〜！ </h1>
+
+      <p className="text-2xl/9 font-medium">You can address me as <span className="font-semibold text-pink-200">Koi</span> under the username of <span className="font-normal bg-mantle px-3 py-1 rounded-lg">@koipix</span>!</p>
+
+    <p className="text-2xl/8 font-medium mt-10">I'm always enthusiastic about the constantly growing technologies and genuinely craving them during my leisure time with a little dip of anime and manga!</p>
+
+    <p className="text-2xl/8 font-medium mt-10"> I used to be the long lost one fellow during my childhood years until my father introduced me to his first PC where I'd just play a few games until hours on end. Here's where my young mind started being curious on how these games actually work behind the scenes.
+    </p>
+    <p className="text-2xl/8 font-medium mt-10">So, I started digging deeper and discovered programming where everything looked like an alien scripture for me so I studied how computer components work with their own responsibilites and a ton of research hence I fell to this inescapable rabbit hole which led me here to this present day~
+    </p>
+    <p className="text-2xl/8 font-medium mt-10">
+      Presently, I've been working on small scale projects while taking Bachelor's degree in Computer Science to understand better and make something for fun out of myself especially game development which is favorable for me, constant research and I also do anime digital art too!  
+    </p>
+  </>
   ) 
 }
 
