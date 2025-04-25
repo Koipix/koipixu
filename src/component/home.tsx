ad3cc2e562df4e29c2861a849a0557db25ad81a9
Koipix <koi.devt@gmail.com>
2025-04-25 17:35:00 +0800

    disable artwork section
    for now..

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -104,25 +104,25 @@ function Home() {
             </div>
           </dl>
         </section>
-        <section className="mt-15">
-          <h1 className="text-3xl mb-5">Artworks</h1>
-            <div className="carousel w-full sm:flex sm:flex-cols-2 sm:gap-6 sm:justify-center">
-              <div id="slide1" className="carousel-item relative w-full sm:h-135 sm:w-100">
-                <img className="w-full rounded-lg" src="/imgs/art-1.png"/>
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between sm:hidden">
-                  <a href="#slide4" className="btn btn-circle bg-pink-200 text-mantle p-5">❮</a>
-                  <a href="#slide2" className="btn btn-circle bg-pink-200 text-mantle p-5">❯</a>
-                </div>
-              </div>
-              <div id="slide2" className="carousel-item relative w-full sm:h-135 sm:w-105">
-                <img className="w-full rounded-lg" src="/imgs/art-2.png"/>
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between sm:hidden">
-                  <a href="#slide1" className="btn btn-circle bg-pink-200 text-mantle p-5">❮</a>
-                  <a href="#slide3" className="btn btn-circle bg-pink-200 text-mantle p-5">❯</a>
-                </div>
-              </div>
-            </div>
-        </section>
+        // <section className="mt-15">
+        //   <h1 className="text-3xl mb-5">Artworks</h1>
+        //     <div className="carousel w-full sm:flex sm:flex-cols-2 sm:gap-6 sm:justify-center">
+        //       <div id="slide1" className="carousel-item relative w-full sm:h-135 sm:w-100">
+        //         <img className="w-full rounded-lg" src="/imgs/art-1.png"/>
+        //         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between sm:hidden">
+        //           <a href="#slide4" className="btn btn-circle bg-pink-200 text-mantle p-5">❮</a>
+        //           <a href="#slide2" className="btn btn-circle bg-pink-200 text-mantle p-5">❯</a>
+        //         </div>
+        //       </div>
+        //       <div id="slide2" className="carousel-item relative w-full sm:h-135 sm:w-105">
+        //         <img className="w-full rounded-lg" src="/imgs/art-2.png"/>
+        //         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between sm:hidden">
+        //           <a href="#slide1" className="btn btn-circle bg-pink-200 text-mantle p-5">❮</a>
+        //           <a href="#slide3" className="btn btn-circle bg-pink-200 text-mantle p-5">❯</a>
+        //         </div>
+        //       </div>
+        //     </div>
+        // </section>
         <section className="mt-18">
           <h1 className="text-3xl mb-5">Pookies :3</h1>
           <div className="flex flex-wrap gap-4">
